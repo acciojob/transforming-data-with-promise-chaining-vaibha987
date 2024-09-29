b496d3226d3a0e871666d2ef9b8af27ba8ffa63e
vaibha987 <v2goyal95@gmail.com>
2024-09-29 19:39:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const number=document.getElementById("ip").value
+const output=document.getElementById("output")
+const button=document.getElementById("btn")
+button.addEventListener('click',()=>{
+	let promise=new Promise(function(resolve,reject) {
+	setTimeout(()=>{
+		resolve(number)
+	},2000)	
+	})
+	promise.then((number)=>{
+		output.innerHTML=`Result:${number}`
+	})
+})
